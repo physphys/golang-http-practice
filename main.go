733095742699944e3b99a91c007e72e3c11c7b18
ryoma kawahara <ryoma.k.0224@gmail.com>
2020-05-04 17:09:20 +0900

    connect postgresql

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type item struct {
+	ID   int
 	Name string
 	Num  int
 }
@@ -14,11 +18,24 @@ type item struct {
 type todo []item
 
 func index(w http.ResponseWriter, r *http.Request) {
-	todo := []item{
-		item{Name: "curry", Num: 1},
-		item{Name: "milk", Num: 2},
-		item{Name: "egg", Num: 20},
+	db, err := sql.Open("postgres", "user=todo_owner dbname=todo sslmode=disable")
+	checkErr(err)
+
+	rows, err := db.Query("SELECT id, name, num FROM todo")
+	checkErr(err)
+
+	var todo todo
+	for rows.Next() {
+		var id int
+		var name string
+		var num int
+		err = rows.Scan(&id, &name, &num)
+		checkErr(err)
+
+		todo = append(todo,
+			item{ID: id, Name: name, Num: num})
 	}
+
 	t, _ := template.ParseFiles("./views/index.html")
 	t.Execute(w, todo)
 }
@@ -28,5 +45,11 @@ func main() {
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
